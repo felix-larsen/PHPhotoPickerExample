9b6e0fd3d1456ea39e9bec895a0a7a65ccbeba58
Felix Larsen <i4guar@gmail.com>
2020-10-17 13:29:46 +0200

    Initial Commit

---
+++ PHPhotoPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PHPhotoPicker
+//
+//  Created by Felix Houghton-Larsen on 17.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PHPhotoPicker/PHPhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHPhotoPickerApp.swift
+//  PHPhotoPicker
+//
+//  Created by Felix Houghton-Larsen on 17.10.20.
+//
+
+import SwiftUI
+
+@main
+struct PHPhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
